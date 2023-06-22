29885a93753464a21da996624ae3f6c728a38c35
André Campos <andre@dimap.ufrn.br>
2023-06-21 22:27:46 -0300

    Comentarios desnecessários  retirados.

--- main.c
+++ main.c
@@ -22,30 +22,11 @@ void print_usage(char *program_name) {
  * Função principal: ponto de partida do programa.
  */
 int main(int argc, char* argv[]) {
-  /*
-    argc e argv são parâmetros padrões na função main(). Enquanto argc indica quantos
-    argumentos foram passados para o programa na linha de comando, argv contém esses argumentos
-    na ordemem que eles foram passados. argc será sempre >= 1, pois argv[0] terá o nome do programa.
-    Por exemplo, se um programa com `meu_prog` for chamado assim:
-    $ ./meu_prog abc def ghi
-    argc terá o valor 4, indicando que há 4 argumentos, sendo o 1º (argv[0]) o nome do
-    próprio programa ("./meu_prog"), o 2º (argv[1]) será "abc", o 3º (argv[2]) será "def"
-    e, por fim, o 4º (argv[3]) será "ghi."
-  */
   if (argc != 2) {
-    // se não houver 2 argumentos, então o programa está sendo usado incorretamente.
-    // deve-se portanto imprimir como usá-lo.
     print_usage(argv[0]);
   }
   else {
     char *operation = argv[1]; // operação passada no argumento.
-    /*
-    A partir daqui, você deve:
-    1) ler a imagem da entrada-padrão;
-    2) criar uma nova imagem em função da operação informada;
-    3) enviar a imagem criada para a saída-padrão.
-    Obs: faça isso organizando as operações em funções.
-    */
   }
 
   return 0;
